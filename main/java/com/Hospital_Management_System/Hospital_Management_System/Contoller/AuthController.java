865469f4dba30e060062df119042e930212537e8
castial004 <nm6395825@gmail.com>
2026-02-05 22:42:39 +0530

    Added a new contoller in controller layer to handel the login and signup requests

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/AuthController.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/AuthController.java
@@ -1,49 +1,45 @@
 package com.Hospital_Management_System.Hospital_Management_System.Contoller;
 
-import com.Hospital_Management_System.Hospital_Management_System.Entity.UserRequestDto;
-import com.Hospital_Management_System.Hospital_Management_System.Entity.UserResponseDto;
-import com.Hospital_Management_System.Hospital_Management_System.Services.AuthService;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.LoginRequestDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.LoginResponseDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.SignUpResponseDto;
+import com.Hospital_Management_System.Hospital_Management_System.Security.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
-    private final AuthenticationManager authenticationManager;
+
     @PostMapping("/signup")
-    public ResponseEntity<UserResponseDto> userSignup(@RequestBody UserRequestDto userRequestDto){
-        UserResponseDto userResponseDto = authService.createAppUser(userRequestDto);
-        if(userResponseDto!=null){
-            //successfully created a new user
-            return ResponseEntity.ok(userResponseDto);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<SignUpResponseDto> userSignup(@RequestBody LoginRequestDto loginRequestDto){
+        SignUpResponseDto signUpDbUser = authService.createAppUser(loginRequestDto);
+        return ResponseEntity.ok(signUpDbUser);
     }
+
     @PostMapping("/login")
-    public ResponseEntity<String> userLogin(@RequestBody UserRequestDto userRequestDto, HttpServletRequest httpServletRequest){
-        try{
-            Authentication auth =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userRequestDto.getUsername(),userRequestDto.getPassword()));
-            SecurityContext context = SecurityContextHolder.createEmptyContext();
-            context.setAuthentication(auth);
-            httpServletRequest.getSession(true).setAttribute("SPRING_SECURITY_CONTEXT", context);
-            //httpServletRequest.getSession(true).setAttribute("SPRING_SECURITY_CONTEXT",auth);
-            UserDetails userDetail = (UserDetails) auth.getPrincipal();
-            return ResponseEntity.ok("Login successful: "+userDetail.getUsername());
-        }
-        catch (Exception e) {
-                return ResponseEntity.badRequest().body("Invalid Credentials");
-        }
+    public ResponseEntity<LoginResponseDto> userLogin(@RequestBody LoginRequestDto loginRequestDto){
+            LoginResponseDto responseDto = authService.login(loginRequestDto);
+            return ResponseEntity.ok(responseDto);
     }
 }
+//Earlier i was using session for authentication
+//    @PostMapping("/login")
+//    public ResponseEntity<String> userLogin(@RequestBody LoginRequestDto userRequestDto, HttpServletRequest httpServletRequest){
+//        try{
+//            Authentication auth =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userRequestDto.getUsername(),userRequestDto.getPassword()));
+//            SecurityContext context = SecurityContextHolder.createEmptyContext();
+//            context.setAuthentication(auth);
+//            httpServletRequest.getSession(true).setAttribute("SPRING_SECURITY_CONTEXT", context);
+//            //httpServletRequest.getSession(true).setAttribute("SPRING_SECURITY_CONTEXT",auth);
+//            UserDetails userDetail = (UserDetails) auth.getPrincipal();
+//            return ResponseEntity.ok("Login successful: "+userDetail.getUsername());
+//        }
+//        catch (Exception e) {
+//                return ResponseEntity.badRequest().body("Invalid Credentials");
+//        }
+//    }
