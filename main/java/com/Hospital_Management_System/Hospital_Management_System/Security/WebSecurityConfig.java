1f22035c1c5b37ac914c3a393eaff536d67673f9
castial004 <nm6395825@gmail.com>
2026-02-07 02:09:03 +0530

    added a config file related to spring security like security filter chain with bean creation of authentication manager as well for passwordEncoder

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Security/WebSecurityConfig.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Security/WebSecurityConfig.java
@@ -1,34 +1,36 @@
-package com.Hospital_Management_System.Hospital_Management_System.Config;
+package com.Hospital_Management_System.Hospital_Management_System.Security;
 
 import com.Hospital_Management_System.Hospital_Management_System.Services.MyUserDetailServiceImp;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Date;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @AllArgsConstructor
 public class WebSecurityConfig {
     private final MyUserDetailServiceImp myUserDetailServiceImp;
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(csrf->csrf.disable())
-                .sessionManagement(session->session.maximumSessions(1))
+                //.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req->
-                        req.requestMatchers("/auth/**").permitAll()
+                        req.requestMatchers("/public/**","/auth/**").permitAll()
+                                //.requestMatchers("admin/**").hasRole("ADMIN")
+                                //.requestMatchers("/doctor/**").hasAnyRole("DOCTOR","ADMIN")
                                 .anyRequest().authenticated()
 
 
-                );
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
                 //.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
