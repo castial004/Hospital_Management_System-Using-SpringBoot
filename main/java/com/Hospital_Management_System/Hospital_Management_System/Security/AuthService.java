171e8c6d22769335d5ca337c57d958ee575eda76
castial004 <nm6395825@gmail.com>
2026-02-08 18:25:28 +0530

    added a auth service layer

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Security/AuthService.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Security/AuthService.java
@@ -1,31 +1,47 @@
-package com.Hospital_Management_System.Hospital_Management_System.Services;
+package com.Hospital_Management_System.Hospital_Management_System.Security;
 
+import com.Hospital_Management_System.Hospital_Management_System.Dto.SignUpResponseDto;
 import com.Hospital_Management_System.Hospital_Management_System.Entity.AppUser;
-import com.Hospital_Management_System.Hospital_Management_System.Dto.UserRequestDto;
-import com.Hospital_Management_System.Hospital_Management_System.Dto.UserResponseDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.LoginRequestDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.LoginResponseDto;
 import com.Hospital_Management_System.Hospital_Management_System.Repository.AppUserRepo;
+import jakarta.persistence.EntityExistsException;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
+
     private final AppUserRepo appUserRepo;
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
-    public UserResponseDto createAppUser(UserRequestDto userRequestDto){
-        Optional<AppUser> optionalAppUser =  appUserRepo.findByUsername(userRequestDto.getUsername());
-        if(optionalAppUser.isEmpty()){
-            // no such user present therefore create
-            AppUser dbAppUser =  appUserRepo.save(AppUser.builder()
-                    .username(userRequestDto.getUsername())
-                    .password(passwordEncoder.encode(userRequestDto.getPassword()))
-                    .build());
-            return new UserResponseDto(dbAppUser.getId(), dbAppUser.getUsername());
+    private final AuthUtil authUtil;
+
+    public SignUpResponseDto createAppUser(LoginRequestDto userRequestDto){
+        AppUser existingAppUser =  appUserRepo.findByUsername(userRequestDto.getUsername()).orElse(null);
+        if(existingAppUser!=null){
+            throw new EntityExistsException();
         }
-        return null;
+        // no such user present therefore create
+        AppUser dbAppUser =  appUserRepo.save(AppUser.builder()
+                .username(userRequestDto.getUsername())
+                .password(passwordEncoder.encode(userRequestDto.getPassword()))
+                .build());
+        return new SignUpResponseDto(dbAppUser.getId(), dbAppUser.getUsername());
+
+    }
+    public LoginResponseDto login(LoginRequestDto loginRequestDto){
+            Authentication auth =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(),loginRequestDto.getPassword()));
+            AppUser appUser = (AppUser) auth.getPrincipal();
+            String token = authUtil.getAccessToken(appUser);
+            return new LoginResponseDto(token,appUser.getId(), appUser.getUsername());
+
     }
 
 }
