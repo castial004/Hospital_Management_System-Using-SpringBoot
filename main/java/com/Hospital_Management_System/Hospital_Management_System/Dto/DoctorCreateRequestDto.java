0b4d9b92edbe57d9b8245cd495861043e1582e4c
castial004 <nm6395825@gmail.com>
2026-02-10 09:19:28 +0530

    created a dto for doctor request

---
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Dto/DoctorCreateRequestDto.java
@@ -0,0 +1,14 @@
+package com.Hospital_Management_System.Hospital_Management_System.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorCreateRequestDto {
+    private String name;
+    private String email;
+    private String specialization;
+}
