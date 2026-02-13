f823afd7f37a27a1cc87371bc8713ae27c9f392f
castial004 <nm6395825@gmail.com>
2026-02-13 10:40:08 +0530

    added a new query

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Repository/InsuranceRepo.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Repository/InsuranceRepo.java
@@ -2,9 +2,15 @@ package com.Hospital_Management_System.Hospital_Management_System.Repository;
 
 import com.Hospital_Management_System.Hospital_Management_System.Entity.Insurance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface InsuranceRepo extends JpaRepository<Insurance,Long> {
 
+    @Query("select i from Insurance i where i.patient.id = :patientId")
+    Optional<Insurance> findByPatientId(@Param("patientId") Long patientId);
 }
