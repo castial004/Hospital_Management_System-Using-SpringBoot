af43e41a525c6932e9fa5278c0597cf006ed7ea6
castial004 <nm6395825@gmail.com>
2026-02-13 10:41:23 +0530

    remove unwanted exception handeling

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Services/InsuranceService.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Services/InsuranceService.java
@@ -2,9 +2,9 @@ package com.Hospital_Management_System.Hospital_Management_System.Services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import com.Hospital_Management_System.Hospital_Management_System.Dto.InsuranceDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.InsuranceCreateRequestDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.InsuranceResponseDto;
 import com.Hospital_Management_System.Hospital_Management_System.Dto.PatientDto;
 import com.Hospital_Management_System.Hospital_Management_System.Entity.Appointment;
 import com.Hospital_Management_System.Hospital_Management_System.Entity.Insurance;
@@ -25,13 +25,26 @@ public class InsuranceService {
 
 
     @Transactional
-    public InsuranceDto setPatientInsurance(Insurance insurance, Long patientId){
-        Patient patient = patientRepo.findById(patientId).orElseThrow(()->new EntityNotFoundException("Invalid patient id: "));
+    public InsuranceResponseDto setPatientInsurance(InsuranceCreateRequestDto insuranceCreateRequestDto){
+        Patient patient = patientRepo.findById(insuranceCreateRequestDto.getPatientId()).orElseThrow(()->new EntityNotFoundException("Invalid patient id: "));
+            Insurance newInsurance = Insurance.builder()
+                    .patient(patient)
+                    .provider(insuranceCreateRequestDto.getProvider())
+                    .policyNumber(insuranceCreateRequestDto.getPolicyNumber())
+                    .validUntil(insuranceCreateRequestDto.getValidUntil())
+                    .build();
             //insuranceRepo.save(insurance); no need jpa will do because of cascade.all
-            patient.setInsurance(insurance);
-            insurance.setPatient(patient);// for bi direction consistency
+            patient.setInsurance(newInsurance);
+            //insurance.setPatient(patient);// for bi direction consistency
             patientRepo.flush();
-            return new InsuranceDto(insurance.getId(),insurance.getPatient().getId(),insurance.getProvider(),insurance.getPolicyNumber(),insurance.getValidUntil(),insurance.getCreatedAt());
+            return InsuranceResponseDto.builder()
+                    .InsuranceId(newInsurance.getId())
+                    .patientId(newInsurance.getPatient().getId())
+                    .createdAt(newInsurance.getCreatedAt())
+                    .provider(newInsurance.getProvider())
+                    .policyNumber(newInsurance.getPolicyNumber())
+                    .validUntil(newInsurance.getValidUntil())
+                    .build();
     }
     // make better
     @Transactional
@@ -44,5 +57,16 @@ public class InsuranceService {
             appointmentIds.add(a.getId());
         }
         return new PatientDto(patient.getId(),patient.getName(),patient.getEmail(),patient.getBirthDate(),patient.getBloodGroup(),patient.getCreatedAt(),null,appointmentIds);
+    }
+    public InsuranceResponseDto getInsuranceByPatientIdService(Long patientId){
+        Insurance dbInsurance =  insuranceRepo.findByPatientId(patientId).orElseThrow(()-> new EntityNotFoundException());
+        return InsuranceResponseDto.builder()
+                .InsuranceId(dbInsurance.getId())
+                .patientId(dbInsurance.getPatient().getId())
+                .policyNumber(dbInsurance.getPolicyNumber())
+                .provider(dbInsurance.getProvider())
+                .createdAt(dbInsurance.getCreatedAt())
+                .validUntil(dbInsurance.getValidUntil())
+                .build();
     }
 }
