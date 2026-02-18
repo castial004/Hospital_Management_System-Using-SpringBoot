68c9161ccb995e40166aef6d5cd63f00aef487e3
castial004 <nm6395825@gmail.com>
2026-02-18 19:11:05 +0530

    our handler for successfull oauth login class

---
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Security/OAuth2SuccessHandler.java
@@ -0,0 +1,16 @@
+package com.Hospital_Management_System.Hospital_Management_System.Security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+}
