cea8fcded640e4b95ec56c2a75059c3f7bb626a9
castial004 <nm6395825@gmail.com>
2026-02-22 21:32:22 +0530

    enabled  cross origin for my auth controller

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/AuthController.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final AuthService authService;
