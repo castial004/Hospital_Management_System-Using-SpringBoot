6fc0eaadf95fa25cdf185a7892d9c8445e797c39
castial004 <nm6395825@gmail.com>
2026-02-22 21:33:43 +0530

    added new fields in app user entity, auth provider type and provider id

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Repository/AppUserRepo.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Repository/AppUserRepo.java
@@ -1,10 +1,19 @@
 package com.Hospital_Management_System.Hospital_Management_System.Repository;
 
 import com.Hospital_Management_System.Hospital_Management_System.Entity.AppUser;
+import com.Hospital_Management_System.Hospital_Management_System.Entity.Enums.AuthProviderType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AppUserRepo extends JpaRepository<AppUser,Long> {
     Optional<AppUser> findByUsername(String username);
+
+    Optional<AppUser> findByProviderIdAndAuthProviderType(String providerId, AuthProviderType authProviderType);
+
+//    @Query("select a from AppUser a where a.providerId = :id and a.authProviderType = :type")
+//    Optional<AppUser> findByProviderIdAndAuthProviderType(@Param("id")String id, @Param("type")AuthProviderType type);
+
 }
