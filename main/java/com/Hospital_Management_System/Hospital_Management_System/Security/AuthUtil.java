645e37c78be9cfcb3d1279c7bfdb038871f7b838
castial004 <nm6395825@gmail.com>
2026-02-23 20:16:24 +0530

    added slf4g annotation for logging

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Security/AuthUtil.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Security/AuthUtil.java
@@ -1,9 +1,12 @@
 package com.Hospital_Management_System.Hospital_Management_System.Security;
 
 import com.Hospital_Management_System.Hospital_Management_System.Entity.AppUser;
+import com.Hospital_Management_System.Hospital_Management_System.Entity.Enums.AuthProviderType;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -11,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class AuthUtil {
     @Value("${jwt.secretKey}")
     private String jwtSecretKey;
@@ -37,5 +41,46 @@ public class AuthUtil {
                 .parseSignedClaims(token)
                 .getPayload()
                 .getSubject();
+    }
+
+    public AuthProviderType getAuthProviderFromRegistrationId(String id){
+        switch (id.toLowerCase()){
+            case "google":
+                return AuthProviderType.GOOGLE;
+            case "github":
+                return AuthProviderType.GITHUB;
+            case "email":
+                return AuthProviderType.EMAIL;
+            default:
+                throw new IllegalArgumentException("Unsupported oauth2 provider: "+id);
+        }
+    }
+
+    public String getProviderId(OAuth2User oAuth2User, String registrationId) {
+            String providerId =  switch(registrationId.toLowerCase()){
+                    case "google"-> oAuth2User.getAttribute("sub");
+                    case "github"-> oAuth2User.getAttribute("id").toString();
+                    default->{
+                        log.error("unsupported oauth2 provider: ",registrationId);
+                        throw new IllegalArgumentException();
+                    }
+            };
+            if (providerId == null || providerId.isBlank()){
+                log.error("unable to determine provider id for provider: "+registrationId);
+                throw new IllegalArgumentException("unable to determine provider id for OAuth2 login");
+            }
+            return providerId;
+    }
+
+    public String determineUserNameFromOAuth2Login(OAuth2User oAuth2User,String providerId,String registerationId){
+        String email = oAuth2User.getAttribute("email");
+        if(email!=null && !email.isBlank()){
+            return email;
+        }
+        return switch (registerationId.toLowerCase()){
+            case "google" -> oAuth2User.getAttribute("sub");
+            case "github" -> oAuth2User.getAttribute("login");
+            default -> providerId;
+        };
     }
 }
