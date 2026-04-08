530256d1c8ccd19fad90fae3cdadde2e13a9d715
castial004 <nm6395825@gmail.com>
2026-04-08 23:39:17 +0530

    added a admin contoller for handeling admin tast

---
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/Admin_Controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.Hospital_Management_System.Hospital_Management_System.Contoller.Admin_Controller;
+
+import com.Hospital_Management_System.Hospital_Management_System.Dto.DoctorResponseDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.OnBoardingDoctorDto;
+import com.Hospital_Management_System.Hospital_Management_System.Services.DoctorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@AllArgsConstructor
+public class AdminController {
+    private final DoctorService doctorService;
+
+    @PostMapping("/onBoardDoctor")
+    public ResponseEntity<DoctorResponseDto> onBoardNewDoctor(@RequestBody OnBoardingDoctorDto newDoctor){
+        DoctorResponseDto savedDoctor =  doctorService.onBoardNewDoctorService(newDoctor);
+        return ResponseEntity.ok(savedDoctor);
+    }
+}
