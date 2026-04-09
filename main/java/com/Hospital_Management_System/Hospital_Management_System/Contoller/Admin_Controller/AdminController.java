e259b929a5df59ce0d211f9268ca36ab8cea70e7
castial004 <nm6395825@gmail.com>
2026-04-09 20:05:40 +0530

    added a check method for admin access or not

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/Admin_Controller/AdminController.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/Admin_Controller/AdminController.java
@@ -5,10 +5,7 @@ import com.Hospital_Management_System.Hospital_Management_System.Dto.OnBoardingD
 import com.Hospital_Management_System.Hospital_Management_System.Services.DoctorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
@@ -20,5 +17,9 @@ public class AdminController {
     public ResponseEntity<DoctorResponseDto> onBoardNewDoctor(@RequestBody OnBoardingDoctorDto newDoctor){
         DoctorResponseDto savedDoctor =  doctorService.onBoardNewDoctorService(newDoctor);
         return ResponseEntity.ok(savedDoctor);
+    }
+    @GetMapping
+    public ResponseEntity<String> check(){
+        return ResponseEntity.ok("ok");
     }
 }
