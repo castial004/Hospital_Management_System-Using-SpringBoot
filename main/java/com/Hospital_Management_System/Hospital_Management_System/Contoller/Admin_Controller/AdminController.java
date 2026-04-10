279207c38cba311bea85100decc42000a24dfddc
castial004 <nm6395825@gmail.com>
2026-04-10 21:08:55 +0530

    add new method to admin controller

--- main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/Admin_Controller/AdminController.java
+++ main/java/com/Hospital_Management_System/Hospital_Management_System/Contoller/Admin_Controller/AdminController.java
@@ -2,8 +2,12 @@ package com.Hospital_Management_System.Hospital_Management_System.Contoller.Admi
 
 import com.Hospital_Management_System.Hospital_Management_System.Dto.DoctorResponseDto;
 import com.Hospital_Management_System.Hospital_Management_System.Dto.OnBoardingDoctorDto;
+import com.Hospital_Management_System.Hospital_Management_System.Dto.PatientDto;
 import com.Hospital_Management_System.Hospital_Management_System.Services.DoctorService;
+import com.Hospital_Management_System.Hospital_Management_System.Services.PatientService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AdminController {
     private final DoctorService doctorService;
+    private final PatientService patientService;
 
     @PostMapping("/onBoardDoctor")
     public ResponseEntity<DoctorResponseDto> onBoardNewDoctor(@RequestBody OnBoardingDoctorDto newDoctor){
@@ -21,5 +26,21 @@ public class AdminController {
     @GetMapping
     public ResponseEntity<String> check(){
         return ResponseEntity.ok("ok");
+    }
+    @GetMapping("/get/doctor/{doctorId}")
+    public ResponseEntity<DoctorResponseDto> getDoctorById(@PathVariable Long doctorId){
+        DoctorResponseDto doctorResponseDto =  doctorService.getDoctorById(doctorId);
+        return ResponseEntity.ok(doctorResponseDto);
+    }
+
+    @GetMapping("/get/patient/{id}")
+    public ResponseEntity<PatientDto> getPatientById(@PathVariable Long id){
+        try{
+            PatientDto patientDto =  patientService.getPatientById(id);
+            return ResponseEntity.ok(patientDto);
+        }
+        catch (EntityNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
